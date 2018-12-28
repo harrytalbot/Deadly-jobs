37ec03c73e9c563d78e71e29fd9dfb548e106d18
hwt1g15 <hwt1g15@ecs.soton.ac.uk>
2018-12-28 13:20:14 +0000

    filtered data and refactored

---
+++ constants.js
@@ -0,0 +1,19 @@
+
+const causes = ["f_trans_rate", "f_violence_rate", "f_fireExp_rate", "f_fallSlipTrip_rate", "f_exposure_rate", "f_contact_rate", "f_allOther_rate"]
+
+const RED = "#e41a1c";
+const BLUE = "";
+const GREEN = "";
+const PURPLE = "";
+const ORANGE = "";
+const PINK = "";
+const GREY = "";
+
+const C1 = "#b2182b"
+const C2 = "#ef8a62"
+const C3 = "#fddbc7"
+const C4 = "#f7f7f7"
+const C5 = "#d1e5f0"
+const C6 = "#67a9cf"
+const C7 = "#2166ac"
+
--- stacked_bar.js
+++ stacked_bar.js
@@ -1,50 +1,28 @@
 /////////////////////////////////////////////////////////////////////////////////////////
-
-
 var dataset;
-
-const causes = ["f_trans_rate", "f_violence_rate", "f_fireExp_rate", "f_fallSlipTrip_rate", "f_exposure_rate", "f_contact_rate", "f_allOther_rate"]
 var firstCause = 0;
 
 const margin = { top: 20, right: 20, bottom: 30, left: 300 };
-const width = 1000;
-const height = 10000;
-
-//Colours
-
-const RED = "#e41a1c";
-const BLUE = "";
-const GREEN = "";
-const PURPLE = "";
-const ORANGE = "";
-const PINK = "";
-const GREY = "";
-
-const C1 = "#b2182b"
-const C2 = "#ef8a62"
-const C3 = "#fddbc7"
-const C4 = "#f7f7f7"
-const C5 = "#d1e5f0"
-const C6 = "#67a9cf"
-const C7 = "#2166ac"
+const stacked_width = 1000;
+const stacked_height = 4000;
 
 /////////////////////////////////////////////////////////////////////////////////////////
+var svg_stacked = d3.select("#svgStacked");
 
-svg = d3.select('body')
+svg_stacked = d3.select('body')
     .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
+    .attr('width', stacked_width + margin.left + margin.right)
+    .attr('height', stacked_height + margin.top + margin.bottom)
     .attr("class", "graph-svg-component"); // BACKGROUND COLOUR
 
-
-g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+g = svg_stacked.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // set y scale
-var y = d3.scaleBand().range([0, height])
+var stacked_y = d3.scaleBand().range([0, stacked_height])
 // set x scale
-var x = d3.scaleLinear().range([0, width]);
+var stacked_x = d3.scaleLinear().range([0, stacked_width]);
 // set the colors                   
-var z = d3.scaleOrdinal().range([C1, C2, C3, C4, C5, C6, C7]);
+var stacked_z = d3.scaleOrdinal().range([C1, C2, C3, C4, C5, C6, C7]);
                                   
 
 // load the csv and create the chart
@@ -63,45 +41,57 @@ d3.csv("data/data.csv", function (data, i) {
     data.f_total_rate = +data.f_violence_rate + +data.f_trans_rate + +data.f_fireExp_rate +
         +data.f_fallSlipTrip_rate + +data.f_exposure_rate + +data.f_contact_rate + +data.f_allOther_rate;
 
+
+    data.nf_violence_rate = +data.nf_violence / +data.totEmp * +100000
+    data.nf_trans_rate = +data.nf_trans / +data.totEmp * +100000
+    data.nf_fireExp_rate = +data.nf_fireExp / +data.totEmp * +100000
+    data.nf_fallSlipTrip_rate = +data.nf_fallSlipTrip / +data.totEmp * +100000
+    data.nf_exposure_rate = +data.nf_exposure / +data.totEmp * +100000
+    data.nf_contact_rate = +data.nf_contact / +data.totEmp * +100000
+    data.nf_allOther_rate = +data.nf_allOther / +data.totEmp * +100000
+
+    data.nf_total_rate = +data.nf_violence_rate + +data.nf_trans_rate + +data.nf_fireExp_rate +
+        +data.nf_fallSlipTrip_rate + +data.nf_exposure_rate + +data.nf_contact_rate + +data.nf_allOther_rate;
+
     return data;
 }, function (error, data) {
-
     if (error) throw error;
 
-    // initially sort the data by total descending
-    data.sort((a, b) => d3.descending(a.f_total_rate, b.f_total_rate));
-    y.domain(data.map(function (d) { return d.occupation; }));
-    x.domain([0, d3.max(data, function (d) { return d.f_total_rate; })]).nice();
-    z.domain(causes);
-
+    // store globally
     dataset = data;
 
+    initData();
     drawChart();
     drawAxis();
     drawLegend();
 });
 
-// function to return an order, using the currently selected firstCause
-function order(data) {
+
+// return a stack order, using the currently selected firstCause
+function getStackedOrder(data) {
     var orderNew = [+0, +1, +2, +3, +4, +5, +6];
+    if (firstCause == -1){ return orderNew}
     orderNew.splice(firstCause, 1);
     orderNew.unshift(firstCause);
     return orderNew;
 }
 
-// Method to update the stacked bar, sorting by a specific cause and running transitions
+// update the stacked bar, sorting by a specific cause and running transitions
 function sortStackedBar(fCause) {
     firstCause = fCause;
-    dataset = [...dataset];
-    
+    var sortFn;
     // define the sort function
-    var sortFn = (a, b) => d3.descending(a[causes[firstCause]], b[causes[firstCause]]);
+    if (firstCause !== -1){
+        sortFn = (a, b) => d3.descending(a[causes[firstCause]], b[causes[firstCause]]);
+    } else {
+        sortFn = (a, b) => d3.descending(a.f_total_rate, b.f_total_rate);
+    }
     // sort the y domain by the chosen cause using sortFn
-    const yCopy = y.domain(dataset.sort(sortFn).map(d => d.occupation)).copy();
+    const yCopy = stacked_y.domain(dataset.sort(sortFn).map(d => d.occupation)).copy();
 
     const groups = d3.selectAll("g.bar-group")
-        .data(d3.stack().keys(causes).order(order)(dataset))
-        .attr("fill", function (d) { return z(d.key); });
+        .data(d3.stack().keys(causes).order(getStackedOrder)(dataset))
+        .attr("fill", function (d) { return stacked_z(d.key); });
 
     const bars = groups.selectAll(".bar")
         .data(d => d, d => d.data.occupation)
@@ -114,7 +104,7 @@ function sortStackedBar(fCause) {
     t0.selectAll("g.bar-group")
         .duration(1000)
         .attr("opacity", function (d) {
-            return (d.key !== causes[firstCause]) ? 0.5 : 1;
+            return (d.key !== causes[firstCause] && firstCause !== -1) ? 0.5 : 1;
         })
 
     var t1 = t0.transition();
@@ -122,7 +112,7 @@ function sortStackedBar(fCause) {
     t1.selectAll("g.bar-group")
         .selectAll(".bar")
         .attr("x", function (d) {
-            return x(d[0]);
+            return stacked_x(d[0]);
         })
 
     var t2 = t1.transition();
@@ -133,35 +123,46 @@ function sortStackedBar(fCause) {
 
     // sort label y axis
     t2.select(".axisY")
-        .call(d3.axisLeft(y))
+        .call(d3.axisLeft(stacked_y))
         .selectAll("g")
 }
 
-// Method to initially draw the chart
+// setup data, scales and filter
+function initData(){
+    dataset = dataset.filter(function(d){ return (d.end_0 == 'TRUE' &&  d.end_00 == 'FALSE')})
+    // initially sort the data by total descending
+    dataset.sort((a, b) => d3.descending(a.f_total_rate, b.f_total_rate));
+    stacked_y.domain(dataset.map(function (d) { return d.occupation; }));
+    stacked_x.domain([0, d3.max(dataset, function (d) { return d.f_total_rate; })]).nice();
+    stacked_z.domain(causes);
+}
+
+// initially draw the chart
 function drawChart() {
     console.log(dataset)
+    
+
     g.append("g")
         .selectAll("g")
         .data(d3.stack().keys(causes).order(d3.stackOrderAscending)(dataset))
         .enter().append("g")
             .classed("bar-group", true)
-            .attr("fill", function (d) { return z(d.key); })
-            .attr("opacity", 1)
+            .attr("fill", function (d) { return stacked_z(d.key); })
+            .attr("opacity", 1) // so first fade animation is smooth
         .selectAll("rect")
-            .data(function (d) { return d; })
-            .enter().append("rect")
+        .data(function (d) { return d; })
+        .enter().append("rect")
             .classed("bar", true)
-            .attr("cause-type", function(d,i) { return 'hello'; })
             .attr("y", function (d) {
-                return y(d.data.occupation);
+                return stacked_y(d.data.occupation);
             })
             .attr("x", function (d) {
-                return x(d[0]);
+                return stacked_x(d[0]);
             })
             .attr("width", function (d) {
-                return x(d[1]) - x(d[0]);
+                return stacked_x(d[1]) - stacked_x(d[0]);
             })
-            .attr("height", y.bandwidth())
+            .attr("height", stacked_y.bandwidth())
         /*.on("mouseover", function () { tooltip.style("display", null); })
         .on("mouseout", function () { tooltip.style("display", "none"); })
         .on("mousemove", function (d) {
@@ -178,12 +179,12 @@ function drawAxis() {
 
     g.append("g")
         .attr("class", "axisY")
-        .call(d3.axisLeft(y));
+        .call(d3.axisLeft(stacked_y));
 
     g.append("g")
         .attr("class", "axisY")
-        .call(d3.axisBottom(x))
-        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(stacked_x))
+        .attr("transform", "translate(0," + stacked_height + ")")
 
     }
 
@@ -199,16 +200,17 @@ function drawLegend() {
         .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
 
     legend.append("rect")
-        .attr("x", width - 19)
+        .attr("x", stacked_width - 19)
         .attr("width", 19)
         .attr("height", 19)
-        .attr("fill", z);
+        .attr("fill", stacked_z);
 
     legend.append("text")
-        .attr("x", width - 24)
+        .attr("x", stacked_width - 24)
         .attr("y", 9.5)
         .attr("dy", "0.32em")
         .attr("fill", 'white')
         .text(function (d) { return d; });
 }
+
 
